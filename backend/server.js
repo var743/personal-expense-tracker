2c35c0f8ede5402895157bb2f5893821da33b7b8
varshini mahalingam <varshinimahalingam5@gmail.com>
2026-06-20 17:41:29 +0530

    server frontend files

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
 const Expense = require("./models/Expense");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "../")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../index.html"));
+});
 mongoose.connect("mongodb://127.0.0.1:27017/expenseTracker")
 .then(() => {
     console.log("Database Connected");
